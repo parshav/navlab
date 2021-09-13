6c07708c6de4bcc2b9fb2a669be9bec295f5ff46
Parshav <pchauhan@clientresourcesinc.com>
2021-09-13 16:04:27 -0500

    Added unneeded back nav animation on button

--- app/src/main/java/com/pv/navigationlab/screens/HomeScreen.kt
+++ app/src/main/java/com/pv/navigationlab/screens/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.pv.navigationlab.screens
 
 import android.util.Log
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -8,13 +13,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.pv.navigationlab.toggle
@@ -29,8 +34,14 @@ fun HomeScreen() {
     val backStackState = navController.currentBackStackEntryAsState()
     val isNested: Boolean = !Routes.Home.contains(backStackState.value?.destination?.route)
 
-    Log.d("pv", "isNested $isNested")
-
+    val inf = rememberInfiniteTransition()
+    val angle by inf.animateFloat(
+        initialValue = 0F, targetValue = 360F, animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000
+            )
+        )
+    )
 
     Scaffold(
         bottomBar = {
@@ -41,7 +52,11 @@ fun HomeScreen() {
                 backgroundColor = Color.Green
             ) {
                 Button(
-                    modifier = Modifier.size(32.dp),
+                    modifier = Modifier
+                        .size(32.dp)
+                        .rotate(
+                            if (isNested) angle else 0f
+                        ),
                     onClick = {
                         if (isNested) {
                             navController.popBackStack()
